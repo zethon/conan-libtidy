ca202bf930cc26844bb74ed42160402082ab845e
Addy <aclaure@gmail.com>
2019-12-10 20:17:05 -0500

    ok

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,18 @@
-from conans import ConanFile, CMake, tools
+# tidy-html5 conand recipe 
+# as used by https://github.com/zethon/Owl
 
+from conans import ConanFile, CMake, tools
 
 class LibtidyConan(ConanFile):
     name = "tidy-html5"
-    version = "5.6"
+    version = "5.7.28"
     url = "https://github.com/htacg/tidy-html5"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
 
-    scm = {"revision": "3a30f6a4300417674026f6dddea5973debc6b808",
+    scm = {"revision": "d1b906991a7587688d384b648c55731f9be52506",
            "type": "git",
            "url": "https://github.com/htacg/tidy-html5.git",
            "subfolder": "source"}
@@ -21,8 +23,8 @@ class LibtidyConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="source/include")
+        self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
