20e86eb809771133e68b10aec6dd218d3cde86e3
Addy <aclaure@gmail.com>
2019-12-18 18:52:24 -0500

    disabled tidy-html5 debug output for Windows builds

--- README.md
+++ README.md
@@ -2,9 +2,11 @@ https://docs.conan.io/en/latest/developing_packages/package_dev_flow.html#conan-
 
 ```
 conan source .
-conan install . -if=build
+conan install . -if=build -s build_type=Debug
 conan build . -bf=build
 conan package . -sf=source -bf=build -pf=package
 conan export-pkg . tidy-html5/5.7.28@owl/stable
 conan upload tidy-html5/5.7.28@owl/stable -r owl
 ```
+
+$ cmake ../source -DDISABLE_DEBUG_LOG=1
--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,18 @@
 # tidy-html5 conan recipe 
 # as used by https://github.com/zethon/Owl
 
+# NOTE: The tidy-html5 library will turn on debug logging for Windows DEBUG builds
+# even if you set `ENABLE_DEBUG_LOG=Off` in the CMake file. This is because there
+# is code to EXPLICITLY turn it on if it's Windows and Debug mode. However, you
+# can use the preprocessor definition `DISABLE_DEBUG_LOG` to turn it off in Windows.
+# But amazingly enough, there's no easy way to pass a preprocessor definition
+# on the command-line to MSBuild. The best way I've found to do this is to set the
+# 'CL' env var, which MSBuild checks for additional command line arguments.
+#
+# See: https://docs.conan.io/en/latest/reference/build_helpers/cmake.html#configure
+#      https://github.com/htacg/tidy-html5/issues/852
+#      https://stackoverflow.com/questions/8564337/how-to-define-a-c-preprocessor-macro-through-the-command-line-with-cmake
+
 import os
 from conans import ConanFile, CMake, tools
 
@@ -24,6 +36,8 @@ class LibtidyConan(ConanFile):
         cmake = CMake(self)
         cmake.definitions["BUILD_SHARED_LIB"] = False
         cmake.configure(source_folder="source")
+        if self.settings.os == "Windows":
+            os.environ["CL"] = "/DDISABLE_DEBUG_LOG=1"
         cmake.build()
 
     def package(self):
